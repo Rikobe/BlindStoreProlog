99f74d179bc1db584bac192181cc3df5931b80aa
Eduardo Cota <eduardocc.8.20@gmail.com>
2019-12-08 18:56:19 -0700

    Qué verguenza

---
+++ mysql.pl
@@ -0,0 +1,207 @@
+
+
+abrir_conexion:-
+    odbc_connect('swiprolog',_,
+                 [user(u839509496_blind),
+                  password('admin123'),
+                  alias(swiprolog),
+                  open(once)]).
+
+cerrar_conexion:-
+    odbc_disconnect('swiprolog').
+
+
+%cadenaIns(Nombre,Precio,CategoriaId,D):-
+%    concat(Nombre,'","',Y),concat(Y,Precio,Z),concat(Z,'","',W),
+%    concat(W,CategoriaID,X), %%Concatenar los atributos
+%    concat('INSERT INTO producto(nombre,precio,categoria_id)
+%    VALUES("',X,E),
+%    concat(E,'")',D).
+
+
+esPadre('Inicio',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM pasillo WHERE IdPasillo = 1',row(X)),
+    cerrar_conexion.
+esPadre('Inicio',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM pasillo WHERE IdPasillo = 2',row(X)),
+    cerrar_conexion.
+esPadre('Inicio',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM pasillo WHERE IdPasillo = 3',row(X)),
+    cerrar_conexion.
+esPadre('Inicio',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM pasillo WHERE IdPasillo = 4',row(X)),
+    cerrar_conexion.
+esPadre('Inicio',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM pasillo WHERE IdPasillo = 5',row(X)),
+    cerrar_conexion.
+
+esPadre('Pasillo 1',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM categoria WHERE IdCategoria = 1',row(X)),
+    cerrar_conexion.
+esPadre('Pasillo 1',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM categoria WHERE IdCategoria = 2',row(X)),
+    cerrar_conexion.
+esPadre('Pasillo 2',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM categoria WHERE IdCategoria = 3',row(X)),
+    cerrar_conexion.
+esPadre('Pasillo 2',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM categoria WHERE IdCategoria = 4',row(X)),
+    cerrar_conexion.
+esPadre('Pasillo 3',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM categoria WHERE IdCategoria = 5',row(X)),
+    cerrar_conexion.
+esPadre('Pasillo 4',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM categoria WHERE IdCategoria = 6',row(X)),
+    cerrar_conexion.
+esPadre('Pasillo 4',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM categoria WHERE IdCategoria = 7',row(X)),
+    cerrar_conexion.
+esPadre('Pasillo 5',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM categoria WHERE IdCategoria = 8',row(X)),
+    cerrar_conexion.
+esPadre('Lácteos',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 1',row(X)),
+    cerrar_conexion.
+esPadre('Lácteos',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 2',row(X)),
+    cerrar_conexion.
+esPadre('Carnes',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 3',row(X)),
+    cerrar_conexion.
+esPadre('Carnes',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 16',row(X)),
+    cerrar_conexion.
+esPadre('Carnes',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 18',row(X)),
+    cerrar_conexion.
+
+esPadre('Fruta y verduras',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 4',row(X)),
+    cerrar_conexion.
+esPadre('Fruta y verduras',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 5',row(X)),
+    cerrar_conexion.
+esPadre('Cereales',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 6',row(X)),
+    cerrar_conexion.
+esPadre('Cereales',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 19',row(X)),
+    cerrar_conexion.
+esPadre('Enlatados',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 7',row(X)),
+    cerrar_conexion.
+esPadre('Enlatados',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 8',row(X)),
+    cerrar_conexion.
+esPadre('Enlatados',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 9',row(X)),
+    cerrar_conexion.
+esPadre('Higiene',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 10',row(X)),
+    cerrar_conexion.
+esPadre('Higiene',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM producto WHERE id_producto = 11',row(X)),
+    cerrar_conexion.
+esPadre('Limpieza',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM Producto WHERE id_producto = 12',row(X)),
+    cerrar_conexion.
+esPadre('Limpieza',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM Producto WHERE id_producto = 20',row(X)),
+    cerrar_conexion.
+esPadre('Medicina',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM id_producto WHERE IdProducto = 15',row(X)),
+    cerrar_conexion.
+esPadre('Medicina',X):-
+    abrir_conexion,
+    odbc_query('swiprolog',
+               'SELECT nombre FROM id_producto WHERE IdProducto = 21',row(X)),
+    cerrar_conexion.
+
+
+
+arbol(['Inicio'|[Arbol]]):-
+    insertarElemento(Arbol,'Pasillo 1').
+
+
+insertarElemento([Arbol],Elemento):-
+    nth0('Inicio',Arbol,Elemento).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
